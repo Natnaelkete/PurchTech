18c72e454efd5486826f700d4b19f33a8e1940ae
NatnaleKete <ketemanatnael023@gmail.com>
2024-05-22 14:31:55 -0700

    update server.js

--- backend/server.js
+++ backend/server.js
@@ -1,22 +1,3 @@
-// import path from "path";
-// import express from "express";
-// import dotenv from "dotenv";
-// dotenv.config();
-// import cors from "cors";
-// import mongoose from "mongoose";
-// import productRoutes from "./routes/productRoutes.js";
-// import userRoutes from "./routes/userRoutes.js";
-// import orderRoutes from "./routes/orderRoutes.js";
-// import chapaRoutes from "./routes/chapaRoutes.js";
-// import { notFound, errorHandler } from "./middleware/errorMiddleware.js";
-// import cookieParser from "cookie-parser";
-// import uploadRoutes from "./routes/uploadRoutes.js";
-
-// const port = 3000;
-
-// const app = express();
-// app.use(
-//   cors({
 import path from "path";
 import express from "express";
 import dotenv from "dotenv";
@@ -27,14 +8,16 @@ import productRoutes from "./routes/productRoutes.js";
 import userRoutes from "./routes/userRoutes.js";
 import orderRoutes from "./routes/orderRoutes.js";
 import chapaRoutes from "./routes/chapaRoutes.js";
-import { notFound, errorHandler } from "./middleware/errorMiddleware.js";
 import uploadRoutes from "./routes/uploadRoutes.js";
+import { notFound, errorHandler } from "./middleware/errorMiddleware.js";
 
+// Load environment variables
 dotenv.config();
 
 const app = express();
 const port = process.env.PORT || 3000;
 
+// Middleware
 app.use(
   cors({
     origin: "http://localhost:5173",
@@ -45,27 +28,35 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 
+// Connect to MongoDB
 mongoose
   .connect(process.env.MONGO_URL)
-  .then(() => console.log("MongoDb connected successfully"))
+  .then(() => console.log("MongoDB connected successfully"))
   .catch((err) => console.log(err));
 
+// API routes
 app.use("/api/products", productRoutes);
 app.use("/api/users", userRoutes);
 app.use("/api/orders", orderRoutes);
 app.use("/api/upload", uploadRoutes);
 app.use("/api/acceptPayment", chapaRoutes);
+
+// Error handling middleware
 app.use(notFound);
 app.use(errorHandler);
 
+// Serving static files in production
 if (process.env.NODE_ENV === "production") {
   const __dirname = path.resolve();
-  app.use("/uploads", express.static("/var/data/uploads"));
+  app.use(
+    "/uploads",
+    express.static(path.join(__dirname, "/var/data/uploads"))
+  );
   app.use(express.static(path.join(__dirname, "/frontend/build")));
 
-  app.get("*", (req, res) =>
-    res.sendFile(path.resolve(__dirname, "frontend", "build", "index.html"))
-  );
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "frontend", "build", "index.html"));
+  });
 } else {
   const __dirname = path.resolve();
   app.use("/uploads", express.static(path.join(__dirname, "/uploads")));
@@ -74,6 +65,7 @@ if (process.env.NODE_ENV === "production") {
   });
 }
 
+// Start the server
 app.listen(port, () => {
   console.log(
     `Server is running in ${process.env.NODE_ENV} mode on port ${port}`
