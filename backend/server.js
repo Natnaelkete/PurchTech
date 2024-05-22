84bc6f155b0a9b8c63c6a3174407b2988723908b
NatnaleKete <ketemanatnael023@gmail.com>
2024-05-22 14:19:56 -0700

    update server.js

--- backend/server.js
+++ backend/server.js
@@ -33,7 +33,7 @@ import uploadRoutes from "./routes/uploadRoutes.js";
 dotenv.config();
 
 const app = express();
-const port = process.env.PORT || 18012;
+const port = process.env.PORT || 3000;
 
 app.use(
   cors({
